e42aad845a817064188658d177426642d77b43cd
BeomChul Kim <2017253083@yonsei.ac.kr>
2021-08-12 20:45:33 +0900

    temp 01
    temp 01

--- temp/temp01.py
+++ temp/temp01.py
@@ -1,18 +1,15 @@
+# [(1, 3), (5, 8), (4, 10), (20, 25)] 가 주어지면, [(1, 3), (4, 10), (20, 25)]
+
 intervals2 = [(1, 3), (5, 8), (4, 10), (20, 25)]
 intervals = [(1, 3), (5, 8), (4, 10), (20, 25), (22, 27)]
-data = []
- 
-intervals = sorted(intervals, key = lambda x: x[0])
- 
+ans = []
+
+intervals = sorted(intervals, key= lambda x: x[0])
+
 for item in intervals:
-    if data and data[-1][1] >= item[1] or data and data[-1][1] >= item[0]:
-        data[-1] = (data[-1][0] , max(item[1] , data[-1][1]))
-    else:
-        data.append(item)
-    
-    
-print(data)
- 
- # 1. ans [(1, 3), (4, 10), (20, 27)]
- # 2. ans [(1, 3), (4, 10), (20, 25)]
- 
+    if ans and ans[-1][1] > item[1] or ans and ans[-1][1] > item[0]:
+        ans[-1] = (ans[-1][0], max(ans[-1][1] , item[1]))
+        continue
+    ans.append(item)
+
+print(ans)
